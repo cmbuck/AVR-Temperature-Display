8dc9bb142ca9e23c44721fa38269466ffe8c5444
cmbuck <cmbuck@vt.edu>
2013-06-18 16:28:57 -0500

    Added serial functions

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 //temperature sensor readings by
 #define CALIBRATION	(-10)
 
+#define USART_BAUDRATE 2400
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
 /*
  * This lookup matrix is the pattern of bits to illuminate
  * The higher byte is the left digit, lower byte is the right digit
@@ -87,6 +90,29 @@ void displayNumber(int number)
 void initializeTimer()
 {
 	
+}
+
+void initializeSerial()
+{
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);   // Turn on the transmission and reception circuitry
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01); // Use 8-bit character sizes
+
+	UBRR0H = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
+	UBRR0L = BAUD_PRESCALE; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
+}
+
+unsigned char serialReadByte()
+{
+	unsigned char receivedByte;
+	while ((UCSR0A & (1 << RXC0)) == 0) {}; // Do nothing until data have been received and is ready to be read from UDR
+	      receivedByte = UDR0; // Fetch the received byte value into the variable "ByteReceived"
+	return receivedByte;
+}
+
+void serialWriteByte(unsigned char byteToSend)
+{
+	while ((UCSR0A & (1 << UDRE0)) == 0) {}; // Do nothing until UDR is ready for more data to be written to it
+	      UDR0 = byteToSend; // Echo back the received byte back to the computer
 }
 
 // initialize adc
